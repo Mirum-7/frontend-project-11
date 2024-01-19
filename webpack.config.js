f6421bcf7fc4eabf30ac4cfe72810b6e3a2a86d2
Эдуард <mirum7@icloud.com>
2024-01-20 02:54:26 +0300

    add Makefile

--- webpack.config.js
+++ webpack.config.js
@@ -48,7 +48,6 @@ const config = {
         use: [
 					MiniCssExtractPlugin.loader, 
 					'css-loader',
-					// Compiles Sass to CSS
 					"sass-loader",
 				],
       },
@@ -56,9 +55,6 @@ const config = {
         test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
         type: 'asset',
       },
-
-      // Add your rules for custom modules here
-      // Learn more about loaders from https://webpack.js.org/loaders/
     ],
   },
 };
